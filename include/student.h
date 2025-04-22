dd0c16073f2446296e6ec2a471e78a30482fc34f
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-04-22 18:31:07 +0300

    Sukurta bazinė klasė Human

--- include/student.h
+++ include/student.h
@@ -1,10 +1,32 @@
 #pragma once
 
+class Human
+{
+protected:
+    string name_;
+    string surname_;
+
+public:
+    Human(const string &name = "Vardenis", const string &surname = "Pavardenis")
+        : name_(name), surname_(surname) {}
+
+    virtual ~Human()
+    {
+        cout << "[~] Base destructor called for: " << name_ << " " << surname_ << endl;
+    }
+    virtual void print() const = 0;
+
+    // Getters and setters
+    string getName() const { return name_; }
+    string getSurname() const { return surname_; }
+    void setName(const string &newName) { name_ = newName; }
+    void setSurname(const string &newSurname) { surname_ = newSurname; }
+};
+
 // Class that holds student data.
-class Student
+class Student : public Human
 {
 private:
-    string name_, surname_;
     vector<int> marks_;
     int exam_;
     double average_, median_;
@@ -12,16 +34,18 @@ private:
 public:
     // Constructors
     Student(const string &name = "Vardenis", const string &surname = "Pavardenis", int exam = 0)
-        : name_(name), surname_(surname), exam_(exam), average_(0.0), median_(0.0) { marks_.reserve(20); }
+        : Human(name, surname), exam_(exam), average_(0.0), median_(0.0) { marks_.reserve(20); }
 
     // Destructor
-    ~Student() { 
-        cout << "[~] Destructor called for: " << name_ << " " << surname_ << endl; 
+    ~Student()
+    {
+        cout << "[~] Destructor called for: " << name_ << " " << surname_ << endl;
     }
 
     // Copy constructor and move constructor
     Student(const Student &other);
     Student(Student &&other);
+
     // Copy assignment operator and move assignment operator
     Student &operator=(const Student &other);
     Student &operator=(Student &&other);
