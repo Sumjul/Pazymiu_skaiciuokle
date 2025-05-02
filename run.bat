1f998a8493b2f69e27f90efde22824a462541e7f
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-05-02 16:17:33 +0300

    Implimentuoti pirmieji veikiantys testai

--- run.bat
+++ run.bat
@@ -1,4 +1,13 @@
-cmake CMakeLists.txt
-cmake --install .
-cmake --build .
-Debug\Pazymiu_skaiciuokle.exe
+@echo off
+setlocal
+set BUILD_TESTS=OFF
+if not "%1"=="" set BUILD_TESTS=%1
+cmake -B build -S . -DBUILD_TESTS=%BUILD_TESTS%
+cmake --build build
+if "%BUILD_TESTS%"=="ON" (
+    build\tests.exe
+) else (
+    build\Pazymiu_skaiciuokle.exe
+)
+endlocal
+
--- src/tests.cpp
+++ src/tests.cpp
@@ -4,3 +4,36 @@
 #include "../include/headers.h"
 #include "../include/student.h"
 #include "../include/templates.h"
+
+TEST_CASE("Student Class Tests")
+{
+    // Test default constructor
+    Student student1;
+    REQUIRE(student1.getName() == "Vardenis");
+    REQUIRE(student1.getSurname() == "Pavardenis");
+    REQUIRE(student1.getExam() == 0);
+    REQUIRE(student1.getAverage() == 0.0);
+    REQUIRE(student1.getMedian() == 0.0);
+
+    // Test parameterized constructor
+    Student student2("John", "Doe", 5);
+    REQUIRE(student2.getName() == "John");
+    REQUIRE(student2.getSurname() == "Doe");
+    REQUIRE(student2.getExam() == 5);
+    REQUIRE(student2.getAverage() == 0.0);
+    REQUIRE(student2.getMedian() == 0.0);
+
+    // Test copy constructor
+    Student student3 = student2;
+    REQUIRE(student3.getName() == "John");
+    REQUIRE(student3.getSurname() == "Doe");
+    REQUIRE(student3.getExam() == 5);
+    REQUIRE(student3.getAverage() == 0.0);
+    REQUIRE(student3.getMedian() == 0.0);
+
+    // Test move constructor
+    Student student4 = std::move(student2);
+    REQUIRE(student4.getName() == "John");
+    REQUIRE(student4.getSurname() == "Doe");
+    REQUIRE(student4.getExam() == 5);
+}
