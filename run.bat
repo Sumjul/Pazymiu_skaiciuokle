5c4606e46dfc15441800389bd283c308c2ab855c
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-05-02 17:06:03 +0300

    Realizuota veikianti programa su CmakeLists ir atnaujinta naudojimosi instrukcija

--- run.bat
+++ run.bat
@@ -1,13 +1,6 @@
 @echo off
 setlocal
-set BUILD_TESTS=OFF
-if not "%1"=="" set BUILD_TESTS=%1
-cmake -B build -S . -DBUILD_TESTS=%BUILD_TESTS%
+cmake -B build -S . 
 cmake --build build
-if "%BUILD_TESTS%"=="ON" (
-    build\tests.exe
-) else (
-    build\Pazymiu_skaiciuokle.exe
-)
+build\Pazymiu_skaiciuokle.exe
 endlocal
-
