caf5e728384eb01f73539db153aa718908a40278
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-05-02 15:32:54 +0300

    Pridėti catch testų source failai

---
+++ src/tests.cpp
@@ -0,0 +1,6 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch2/catch.hpp"
+#include "../include/global.h"
+#include "../include/headers.h"
+#include "../include/student.h"
+#include "../include/templates.h"
