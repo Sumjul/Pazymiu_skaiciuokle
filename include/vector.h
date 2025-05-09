6bd07a4a8e489339f972ee6e0e82d4680ee1555e
Julius Vilkanec <julius.vilkanec@mif.stud.vu.lt>
2025-05-09 18:21:29 +0300

    Sukurtos kelios papildomos vector funkcijos

--- include/vector.h
+++ include/vector.h
@@ -7,4 +7,10 @@ private:
 public:
     Vector();
     ~Vector();
+    void Push_back(int value);
+    bool Empty() const;
+    int Size() const;
+    int Capacity() const;
+    bool operator==(const Vector& other) const;
+    bool operator!=(const Vector& other) const;
 };
--- src/vector.cpp
+++ src/vector.cpp
@@ -4,11 +4,43 @@
 #include "../include/templates.h"
 #include "../include/vector.h"
 
-Vector::Vector() : size(0), capacity(5), data(new int[capacity])
-{
-}
+Vector::Vector()
+    : size(0), capacity(5), data(new int[capacity]) {}
 
 Vector::~Vector()
 {
     delete[] data;
+}
+
+void Vector::Push_back(int value)
+{
+    data[size] = value;
+    ++size;
+}
+
+bool Vector::Empty() const
+{
+    return size == 0;
+}
+
+int Vector::Size() const
+{
+    return size;
+}
+
+int Vector::Capacity() const
+{
+    return capacity;
+}
+
+bool Vector::operator==(const Vector& other) const
+{
+    if (Size() != other.Size())
+        return false;
+    for (int i = 0; i < Size(); ++i)
+    {
+        if (data[i] != other.data[i])
+            return false;
+    }
+    return true;
 }
